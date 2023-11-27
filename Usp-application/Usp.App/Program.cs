d74ec51b5bbf0582f944ad7c9fcb0fa5b1835ce9
YaroslavKuchuk <yaroslav.kuchuk@gmail.com>
2023-11-27 12:27:44 +0100

    5-web-ui-structure: listener

--- Usp-application/Usp.App/Program.cs
+++ Usp-application/Usp.App/Program.cs
@@ -1,10 +1,19 @@
 using Usp.App.Components;
+using Usp.App.Logic.Services;
+using Usp.App.Logic.Services.Abstractions;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddHttpClient("UspApi", (httpClient) =>
+{
+    httpClient.BaseAddress = new Uri(builder.Configuration["UspApiPath"]);
+});
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
+builder.Services.AddTransient(typeof(ISellingPointService), typeof(SellingPointService));
 
 var app = builder.Build();
 
